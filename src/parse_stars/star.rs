6d0148d973f84e5fccef7dcc4035a7cecc364c72
vasil <vaskonikolov2003@gmail.com>
2022-06-19 18:09:48 +0300

    add functions to calculate angles between stars

--- src/parse_stars/star.rs
+++ src/parse_stars/star.rs
@@ -1,5 +1,5 @@
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Star {
     /// the index of the star, serving as its name. Stars are to be refered to by index
     pub index: u16,
@@ -7,6 +7,16 @@ pub struct Star {
     pub coords: [f64; 3], 
     /// the visual brigthness of the star in magnitudes
     pub brightness: f64,
+}
+
+/// returns the cosine of the angle between two stars
+pub fn cos_angle_between_stars(s1: &Star, s2: &Star) -> f64 {
+    // dot product
+    s1.coords.iter().zip(s2.coords.iter()).map(|(&a, &b)| a*b).sum()
+}
+
+pub fn angle_between_stars(s1: &Star, s2: &Star) -> f64 {
+    cos_angle_between_stars(s1, s2).acos()
 }
 
 impl Star {
