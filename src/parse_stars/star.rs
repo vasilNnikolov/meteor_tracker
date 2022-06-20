c8490d13bc807bfed4e16ee9ec3dce7a607c9d16
vasil <vaskonikolov2003@gmail.com>
2022-06-20 15:47:50 +0300

    refactor coordinates to use nalgebra::vector3

--- src/parse_stars/star.rs
+++ src/parse_stars/star.rs
@@ -1,10 +1,11 @@
+use nalgebra::Vector3;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Star {
     /// the index of the star, serving as its name. Stars are to be refered to by index
     pub index: u16,
     /// the x, y, z coordinates of a star in the earth reference frame
-    pub coords: [f64; 3], 
+    pub coords: Vector3<f64>, 
     /// the visual brigthness of the star in magnitudes
     pub brightness: f64,
 }
@@ -12,7 +13,7 @@ pub struct Star {
 /// returns the cosine of the angle between two stars
 pub fn cos_angle_between_stars(s1: &Star, s2: &Star) -> f64 {
     // dot product
-    s1.coords.iter().zip(s2.coords.iter()).map(|(&a, &b)| a*b).sum()
+    s1.coords.dot(&s2.coords)
 }
 
 pub fn angle_between_stars(s1: &Star, s2: &Star) -> f64 {
@@ -23,7 +24,7 @@ impl Star {
     pub fn new(ra: f64, dec: f64, brightness: f64, index: u16) -> Star {
         Star {
             index,
-            coords: [dec.cos()*ra.cos(), dec.cos()*ra.sin(), dec.sin()],
+            coords: Vector3::new(dec.cos()*ra.cos(), dec.cos()*ra.sin(), dec.sin()),
             brightness
         }     
     }
