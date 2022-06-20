7266218e2a3ff228d48bfba5dd6d1da91a0fa267
vasil <vaskonikolov2003@gmail.com>
2022-06-20 15:48:35 +0300

    test how nalgebra module works

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 mod parse_stars;
 mod coordinates;
 
+use nalgebra::{Vector3, Matrix3};
 fn main() {
-    println!("Hello, world!");
-    if let Err(e) = coordinates::generate_database::generate_db(1.1, 1) {
-        println!("{}", e);
-    }
+     // println!("Hello, world!");
+     // if let Err(e) = coordinates::generate_database::generate_db(1.1, 1) {
+     //     println!("{}", e);
+     // }
+    
+
+    let M = Matrix3::from_columns(&[
+        Vector3::new(1.0, 2.0, 3.0), Vector3::new(4.0, 5.0, 6.0), Vector3::new(7.0, 8.0, 9.0)
+    ]);
+    let v = Vector3::new(1.0, 2.0, 3.0);
+    println!("{:?}", v.shape());
+
+    println!("{:?}", M);
+    println!("{:?}", v.transpose()*M);
 }
