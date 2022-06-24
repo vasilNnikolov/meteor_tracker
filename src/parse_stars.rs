c30b97df6eaab62a5d999afe30f6e3e9e2051b44
vasil <vaskonikolov2003@gmail.com>
2022-06-24 20:10:17 +0300

    add main function of parse_stars module

--- src/parse_stars.rs
+++ src/parse_stars.rs
@@ -1,4 +1,13 @@
+use crate::take_picture;
 pub mod star;
+
+/// takes a Picture struct, and tries to parse it into a vector of Star structs, in the coordinate
+/// system of the camera
+/// if it deems the picture bad (too many clouds, lights etc.), it returns a string with a
+/// corresponding error message
+pub fn parse_image_to_stars(image: take_picture::Picture) -> Result<Vec<star::Star>, String> {
+    Err(String::from("parse_image_to_stars not unimplemented"))
+}
 
 #[cfg(test)]
 mod tests {
