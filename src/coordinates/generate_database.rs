97f3940b319be05bc5c663f09c066482f98887aa
vasil <vaskonikolov2003@gmail.com>
2022-06-24 18:43:52 +0300

    serialise trait not implemented for json, maybe its not worth the work

--- src/coordinates/generate_database.rs
+++ src/coordinates/generate_database.rs
@@ -2,7 +2,17 @@ use crate::parse_stars::star::Star;
 use crate::coordinates::flower;
 use std::fs::File;
 use std::io::{BufReader, prelude::*};
-use rustfft::{FftPlanner, num_complex::Complex}
+use rustfft::{FftPlanner, num_complex::Complex};
+
+// extern crate serde_json;
+// extern crate serde;
+extern crate serde_derive;
+
+#[derive(Debug, serde_derive::Serialize, serde_derive::Deserialize)]
+pub struct DFT_coeffs {
+    r_coeffs: Vec<Complex<f64>>, 
+    delta_coeffs: Vec<Complex<f64>>
+}
 
 /// tries to read HYG db, if successful returns a vector of stars
 /// note: the indexes of the stars in the Star struct start at 1 
@@ -57,23 +67,32 @@ pub fn read_hyd_database(m_lim: f64) -> Result<Vec<Star>, String> {
 pub fn generate_db(m_lim: f64, k: u16, fov: f64) -> Result<(), String>{
     let all_stars = read_hyd_database(m_lim)?; 
 
-    let mut flower_patterns: Vec<flower::FlowerPattern>;
-    let mut r_dft_coefficients: Vec<[f64; k]>;
-    let mut delta_dft_coefficients: Vec<[f64; k]>;
+    let mut fft_planner = FftPlanner::new();
+    let fft = fft_planner.plan_fft_forward(k as usize);
+
+    // create file to write to 
+    let db_location = format!("databases/k{}fov_deg{}.json", k, (fov*180.0/std::f64::consts::PI) as u16);
+    let db_file = match File::create(db_location) {
+        Ok(f) => f, 
+        Err(_) => { return Err(String::from("could not create file to write the dft coefficients to")); }
+    };
+    
     for star in all_stars.iter() {
         let pattern = flower::FlowerPattern::generate(star.index, k, fov, &all_stars)?;
-        flower_patterns.push(pattern);
-
-        let r_values: Vec<Complex<f64>> = pattern.r.iter().map(|&value_f| Complex::new(value_f, 0)).collect()
-        let delta_values: Vec<Complex<f64>> = pattern.delta.iter().map(|&value_f| Complex::new(value_f, 0)).collect()
-
-
+        // these will hold the DFT coefficients after the transformation
+        let mut r_values: Vec<Complex<f64>> = pattern.r.iter().map(|&value_f| Complex::new(value_f, 0.0)).collect();
+        let mut delta_values: Vec<Complex<f64>> = pattern.delta.iter().map(|&value_f| Complex::new(value_f, 0.0)).collect();
+        // generate DFT coefficients of r(i) and delta(i) functions, i in [1..k]
+        fft.process(&mut r_values);
+        fft.process(&mut delta_values);
+        // write DFT coefficients of flower patterns to .csv file
+        let coeff_struct = DFT_coeffs {
+            r_coeffs: r_values, 
+            delta_coeffs: delta_values
+        };
+        
+        
     }
-    // generate DFT coefficients of r(i) and delta(i) functions, i in [1..k]
-     
-
-    // write DFT coefficients of flower patterns to .csv file
-
 
     Ok(())
 }
