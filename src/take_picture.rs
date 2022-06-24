d4b78efe85eb14e12e41ee358e77e3038b8de7f3
vasil <vaskonikolov2003@gmail.com>
2022-06-24 20:09:52 +0300

    add Picture struct and main function of the module

---
+++ src/take_picture.rs
@@ -0,0 +1,10 @@
+
+pub struct Picture {
+
+}
+
+/// takes a picture non-blockingly, and returns a String with an error message if it failed for
+/// some reason
+pub fn take_picture() -> Result<Picture, String> {
+    Err(String::from("take_picture not unimplemented!"))
+}
