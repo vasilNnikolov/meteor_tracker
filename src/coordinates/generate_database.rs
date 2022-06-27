7595bb6f76db0843be3f20be6f6dbdb6d6fdb6b9
vasil <vaskonikolov2003@gmail.com>
2022-06-27 21:11:11 +0300

    remove file writing for DFT database

--- src/coordinates/generate_database.rs
+++ src/coordinates/generate_database.rs
@@ -4,17 +4,12 @@ use std::fs::File;
 use std::io::{BufReader, prelude::*};
 use rustfft::{FftPlanner, num_complex::Complex};
 
-pub struct DFT_coefficients {
-    pub r_coeffs: Vec<Complex<f64>>, 
-    pub delta_coeffs: Vec<Complex<f64>>
-}
-
 /// a struct which will hold the database of dft coefficients for all stars brighter than m_lim
 pub struct DFT_database {
     pub k: u16, 
     pub fov: f64, 
-    pub r_dft_coefficients: Vec<Complex<f64>>, 
-    pub delta_dft_coefficients: Vec<Complex<f64>>, 
+    pub r_dft_coefficients: Vec<Vec<Complex<f64>>>,
+    pub delta_dft_coefficients: Vec<Vec<Complex<f64>>>,
     pub m_lim: f64
 }
 
@@ -66,27 +61,16 @@ pub fn read_hyd_database(m_lim: f64) -> Result<Vec<Star>, String> {
 /// m_lim - the limiting magnitude of the camera
 /// k - the number of outer stars in the flower pattern, approx 10, maybe more
 /// fov - the radius of the field of view of the camera, in radians
-/// It creates a file with N entries, N being the number of stars brighter than m_lim, and for each one stores the DFT coefficients
-/// of r(i) - distance between the central star and star number i of the petels, and delta(i) - angle between the petel i and (i + 1)
-pub fn generate_db(m_lim: f64, k: u16, fov: f64) -> Result<(), String>{
+pub fn generate_db(m_lim: f64, k: u16, fov: f64) -> Result<DFT_database, String>{
     let all_stars = read_hyd_database(m_lim)?; 
 
     let mut fft_planner = FftPlanner::new();
     let fft = fft_planner.plan_fft_forward(k as usize);
 
-    // create file to write to 
-    let db_location = format!("src/coordinates/databases/k{}fov_deg{}.csv", k, (fov*180.0/std::f64::consts::PI) as u16);
-    let mut db_file = match File::create(db_location) {
-        Ok(f) => f, 
-        Err(_) => { return Err(String::from("could not create file to write the dft coefficients to")); }
-    };
+    let mut r_dft_coefficients: Vec<Vec<Complex<f64>>> = vec![];
+    let mut delta_dft_coefficients: Vec<Vec<Complex<f64>>> = vec![];
 
-    // write k and fov on the first two lines
-    if let Err(_) = db_file.write_all(format!("{}\n{}\n", k, fov).as_bytes()) {
-        return Err(String::from("could not write k and fov to db file for some reason"));
-    }
-
-    //compute and write dft coefficients to csv file
+    // compute dft coefficients
     for star in all_stars.iter() {
         let pattern = flower::FlowerPattern::generate(star.index, k, fov, &all_stars)?;
 
@@ -98,33 +82,16 @@ pub fn generate_db(m_lim: f64, k: u16, fov: f64) -> Result<(), String>{
         fft.process(&mut r_values);
         fft.process(&mut delta_values);
 
-        // write DFT coefficients of flower patterns to .csv file
-        write_coeffs_to_file(&r_values, &mut db_file)?;
-        write_coeffs_to_file(&delta_values, &mut db_file)?;
+        r_dft_coefficients.push(r_values); 
+        delta_dft_coefficients.push(delta_values);
     }
 
-    Ok(())
-}
-
-fn write_coeffs_to_file(coeffs: &Vec<Complex<f64>>, file: &mut File) -> Result<(), String> {
-    let mut str_to_write = String::new();
-    for c in coeffs {
-        str_to_write.push_str(format!("{},{},", c.re, c.im).as_str());
-    }
-    str_to_write.push_str("\n");
-    if let Err(_) = file.write_all(str_to_write.as_bytes()) {
-        return Err(String::from("could not write DFT coeffs to file for some reason"));
-    }
-
-    Ok(())
-}
-
-/// tries to find and read a DFT database file for given number of petels and fov
-/// k - number of outer stars for the flower pattern
-/// fov - radius of the fov in radians
-/// returns Err(String) if it could not read the file
-/// if it does not find a file with the 
-pub fn read_dft_database(k: u16, fov: f64) -> Result<Vec<DFT_coefficients>, String> {
-
+    Ok(DFT_database {
+        k, 
+        fov, 
+        r_dft_coefficients, 
+        delta_dft_coefficients, 
+        m_lim
+    })
 }
 
