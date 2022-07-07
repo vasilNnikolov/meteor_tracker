fdf288bcfb98a6a1c31319308b518ce81dd33fea
vasil <vaskonikolov2003@gmail.com>
2022-07-07 19:23:00 +0300

    add testing to see how phase correlation works
    Plot twist - the wiki article is wrong, apparently. The tau is (k -
    peak_position) % k where k is the length of the array on which we
    perform FFT. Tau is such that r'(i) = r(i - tau)

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,35 @@ mod take_picture;
 mod parse_stars;
 mod coordinates;
 
-use nalgebra::{Vector3, Matrix3};
+use rustfft::{FftPlanner, num_complex::Complex};
 fn main() {
-    
-    // let M = Matrix3::from_columns(&[
-    //     Vector3::new(1.0, 2.0, 3.0), Vector3::new(4.0, 5.0, 6.0), Vector3::new(7.0, 8.0, 9.0)
-    // ]);
-    // let v = Vector3::new(1.0, 2.0, 3.0);
-    // println!("{:?}", v.shape());
+    let mut buff: Vec<Complex<f64>> = vec![];
+    let n: i32 = 10;
+    for i in 0..n {
+        buff.push(Complex {
+            re: i as f64, 
+            im: 0.0
+        });
+    }
 
-    // println!("{:?}", M);
-    // println!("{:?}", v.transpose()*M);
+    let mut buff2: Vec<Complex<f64>> = vec![];
+    for i in 0..n {
+        buff2.push(buff[(((i - 3) % n + n) % n) as usize]);
+        println!("{}, {}, {}", i, buff[i as usize].norm(), buff2[i as usize].norm());
+    }
+
+    let mut planner = FftPlanner::new();
+    let fft = planner.plan_fft_forward(n as usize);
+
+    fft.process(&mut buff);
+    fft.process(&mut buff2);
+    let mut R: Vec<Complex<f64>> = vec![];
+    for i in 0..n as usize {
+        R.push(buff[i]*buff2[i].conj()/(buff[i]*buff2[i].conj()).norm());
+    }
+    let inverse_fft = planner.plan_fft_inverse(n as usize);
+    inverse_fft.process(&mut R);
+    for i in 0..n as usize{
+        println!("{}: {}", i, R[i].norm());
+    }
 }
