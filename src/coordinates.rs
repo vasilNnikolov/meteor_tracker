b3c5ac3f3959781cf61abd44c440c2b27e839540
vasil <vaskonikolov2003@gmail.com>
2022-07-07 15:14:51 +0300

    create flower pattern from observation
    almost finished the main function of the module, got the angle of
    rotation of the camera relative to the sky coordinate system

--- src/coordinates.rs
+++ src/coordinates.rs
@@ -1,23 +1,23 @@
 pub mod generate_database;
 pub mod flower;
 use crate::parse_stars::star;
+use crate::coordinates::flower::FlowerPattern;
 use rustfft::{FftPlanner, num_complex::Complex};
-
 
 /// gets the vector of captured stars and a dft database which is pre-calculated at the beginning
 /// of the observations, and tries to compute the rotation matrix of the camera
 /// the return matrix R is such that 
 /// R*x_camera = x_real, where x_camera is the radius-vector of a point in the camera coordinate
 /// system and x_real is the position of the same point in the geocentric coordinate system
-pub fn get_rotation_matrix(captured_stars: &Vec<star::Star>, dft_database: generate_database::DFT_database) -> Result<nalgebra::Matrix3<f64>, String> {
+pub fn get_rotation_matrix(captured_stars: &Vec<star::Star>, dft_database: &generate_database::DFT_database, flower_patterns: &Vec<FlowerPattern>) -> Result<nalgebra::Matrix3<f64>, String> {
     let k = dft_database.r_dft_coefficients[0].len();
     let fov = dft_database.fov;
-    let pattern = flower::FlowerPattern::generate(6969, k as u16, fov, captured_stars)?;
+    let observed_pattern = generate_flower_pattern_from_observation(captured_stars, k as u16, fov)?;
 
     let mut fft_planner = FftPlanner::new();
     let fft = fft_planner.plan_fft_forward(k as usize);
-    let mut r_values: Vec<Complex<f64>> = pattern.r.iter().map(|&value_f| Complex::new(value_f, 0.0)).collect();
-    let mut delta_values: Vec<Complex<f64>> = pattern.delta.iter().map(|&value_f| Complex::new(value_f, 0.0)).collect();
+    let mut r_values: Vec<Complex<f64>> = observed_pattern.r.iter().map(|&value_f| Complex::new(value_f, 0.0)).collect();
+    let mut delta_values: Vec<Complex<f64>> = observed_pattern.delta.iter().map(|&value_f| Complex::new(value_f, 0.0)).collect();
 
     // generate DFT coefficients of r(i) and delta(i) functions, i in [1..k]
     fft.process(&mut r_values);
@@ -43,32 +43,64 @@ pub fn get_rotation_matrix(captured_stars: &Vec<star::Star>, dft_database: gener
         R_rs.push(R_r);
         R_deltas.push(R_delta);
     } 
-    let central_star_true_index: u16;
-    match match_catalogue_star_to_central(R_rs, R_deltas) {
-        Ok(index) => central_star_true_index = index, 
+
+    let (central_star_true_index, tau) = match match_catalogue_star_to_central(R_rs, R_deltas) {
+        Ok((index, t)) => (index, t), 
         Err(s) => {
             // TODO handle error of no matched star better
             return Err(s);
         }
-    }
+    };
 
-    // once we know the central star index, we need the rotation of the camera relative to the Sky
-    // Coordinate system (see README) by checking the actual angles of the observed flower pattern
-    // and comparing them with the expected angles for the central star. The observed angles should
-    // all be shifted some amount
-    // TODO implement ^
+    // angle of petel minus angle of corresponding petel in the catalogue flower pattern
+    let mut average_angle_offset = 0.0;
+    let catalogue_flower_pattern = flower_patterns.get(central_star_true_index as usize).unwrap(); 
+    for petel_index in 0..k as i32 {
+        let corresponding_index_of_observed_star = ((petel_index - tau) % k as i32 + k as i32) % k as i32;
+        average_angle_offset += observed_pattern.angle_of_petel(petel_index as u16).unwrap() - catalogue_flower_pattern.angle_of_petel(corresponding_index_of_observed_star as u16).unwrap();
+    }
+    average_angle_offset /= k as f64;
     
+    // TODO generate matrix from observed star and its corresponding catalogue star and the angle
+    // of rotation of the camera
     Err(String::from("get_rotation_matrix not unimplemented!"))
 }
 
-/// returns the index of the catalogue star which best matches the observed flower pattern
+/// generates a flower pattern from the observed stars, to be used for matching against the DFT
+/// database 
+/// TODO create better implementation
+/// currently it selects the (k + 1) brightest stars, picks the one closest to the center of the
+/// image as the central one, and the other k as petels
+fn generate_flower_pattern_from_observation(captured_stars: &Vec<star::Star>, k: u16, fov: f64) -> Result<FlowerPattern, String> {
+    let mut k_plus_one_brightest: Vec<star::Star> = captured_stars.clone();
+    // sort by lowest magnitude first
+    k_plus_one_brightest.sort_by(|&a, &b| {
+        b.brightness.partial_cmp(&a.brightness).unwrap()
+    });
+    if k + 1 > k_plus_one_brightest.len() as u16 {
+        return Err(format!("there are not enough stars in the image to generate a flower pattern of size k={}", k));
+    }
+    k_plus_one_brightest = k_plus_one_brightest.into_iter().take((k + 1) as usize).collect();
+    // sort them by distance to the center of the image, smallest distance first
+    k_plus_one_brightest.sort_by(|&a, &b| {
+        let a_dist_squared = a.coords.x.powi(2) + a.coords.z.powi(2);
+        let b_dist_squared = b.coords.x.powi(2) + b.coords.z.powi(2);
+        b_dist_squared.partial_cmp(&a_dist_squared).unwrap()
+    });
+    // index is 1 and not 0 because of the implementation of generate, made for index according to
+    // the HYG db
+    Ok(FlowerPattern::generate(1, k, fov, &k_plus_one_brightest)?)
+}
+/// returns the index of the catalogue star which best matches the observed flower pattern, and the
+/// offset tau, for which r'(i) = r((i - tau) % k)
 /// R_rs - a Vec of the same length of the star catalogue, each entry is the R vector corresponding
 /// to the functions r(i) and r'(i) for every star from the catalogue and the central star
 /// R_deltas - same with R_rs but for delta(i) and delta'(i)
 /// if successful, returns Ok(index of the best matching star in the catalogue)
 /// if not, it could not match sufficiently well any star on the catalogue to the central star,
-/// possibly indicating a false central star, or some many false stars as petels
-fn match_catalogue_star_to_central(R_rs: Vec<Vec<Complex<f64>>>, R_deltas: Vec<Vec<Complex<f64>>>) -> Result<u16, String> {
+/// possibly indicating a false central star, or many false stars as petels
+fn match_catalogue_star_to_central(R_rs: Vec<Vec<Complex<f64>>>, R_deltas: Vec<Vec<Complex<f64>>>) -> Result<(u16, i32), String> {
+    // TODO implement!
     Err(String::from("match_catalogue_star_to_central not unimplemented!"))
 }
 
@@ -87,7 +119,7 @@ mod tests {
     #[test]
     fn test_sirius_pattern() {
         let all_stars = generate_database::read_hyd_database(4.0).unwrap();
-        let sirius_pattern = flower::FlowerPattern::generate(1, 5, 0.35, &all_stars).unwrap();
+        let sirius_pattern = FlowerPattern::generate(1, 5, 0.35, &all_stars).unwrap();
         let n = sirius_pattern.outer_stars.len(); 
         for i in 0..n {
             let (ra, dec) = sirius_pattern.outer_stars[i].get_ra_dec();
@@ -103,7 +135,7 @@ mod tests {
     #[test]
     fn test_canopus_pattern() {
         let all_stars = generate_database::read_hyd_database(4.0).unwrap();
-        let canopus_pattern = flower::FlowerPattern::generate(2, 5, 0.35, &all_stars).unwrap();
+        let canopus_pattern = FlowerPattern::generate(2, 5, 0.35, &all_stars).unwrap();
         let n = canopus_pattern.outer_stars.len(); 
         for i in 0..n {
             let (ra, dec) = canopus_pattern.outer_stars[i].get_ra_dec();
