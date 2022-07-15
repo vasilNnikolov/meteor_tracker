3907f606b3e45faa887477b8e32ccf00666c9f58
vasil <vaskonikolov2003@gmail.com>
2022-07-15 15:37:53 +0300

    works only sometimes with random errors in observed stars

--- src/coordinates.rs
+++ src/coordinates.rs
@@ -91,50 +91,7 @@ fn generate_flower_pattern_from_observation(captured_stars: &Vec<star::Star>, k:
 /// possibly indicating a false central star, or many false stars as petels
 /// TODO make a better choice function
 fn match_catalogue_star_to_central(R_rs: &mut Vec<Vec<Complex<f64>>>, R_deltas: &mut Vec<Vec<Complex<f64>>>) -> Result<(u16, u16), String> {
-    // TODO remove comment
-    // {
-    //     let n = R_rs.len();
-    //     let mut planner = FftPlanner::new();
-    //     let k = R_rs[0].len();
-    //     let inverse_fft = planner.plan_fft_inverse(k);
-
-    //     struct BestMatch {
-    //         score: f64, 
-    //         central_star_index: u16, 
-    //         tau: u16
-    //     }
-        
-    //     fn get_score_tau(r: &Vec<Complex<f64>>) -> (f64, u16) {
-    //         let (max_r_index, max_r_value): (usize, f64) = r.iter().enumerate()
-    //             .map(|(i, c)| (i, c.norm()))
-    //             .max_by(|&(_, f1), &(_, f2)| f1.partial_cmp(&f2).unwrap()).unwrap();
-
-    //         let k = r.len();
-    //         let residual_quadratic = r.iter().map(|&c| c.norm().powi(2)).sum::<f64>() - max_r_value.powi(2);
-    //         let score: f64 = 1.0 - (residual_quadratic/(k - 1) as f64).powf(0.5)/max_r_value;
-    //         (score, max_r_index as u16) 
-    //     }
-
-    //     let mut matches: Vec<BestMatch> = vec![];
-    //     for index in 0..n {
-    //         inverse_fft.process(&mut R_rs[index]);
-    //         let (score, tau) = get_score_tau(&R_rs[index]);
-    //         matches.push(BestMatch {
-    //             score,
-    //             central_star_index: index as u16, 
-    //             tau
-    //         });
-    //     }
-    //     // find best score 
-    //     let best_match = matches.iter().max_by(|&m1, &m2| {
-    //         m1.score.partial_cmp(&(m2.score)).unwrap()
-    //     }).unwrap();
-
-    //     println!("score of match: {}", best_match.score);
-
-    //     Ok((best_match.central_star_index, (k as u16 - best_match.tau)%k as u16))
-    // }
-    let p: u16 = 10;
+    let p: u16 = 20;
     let best_matches_r = top_match_candidates(R_rs, p)?;
     let best_matches_delta = top_match_candidates(R_deltas, p)?;
     
@@ -156,7 +113,6 @@ fn match_catalogue_star_to_central(R_rs: &mut Vec<Vec<Complex<f64>>>, R_deltas: 
     // most of the times the matching_star vec will have one element anyways(i hope)
     Ok(matching_star[0])
 }
-
 /// this function looks at an R vector (as in wiki article) and returns the top p candidates for
 /// matches, as per some score function inside this one
 /// returns - a result of Vector of tuples (star_index, tau), the vector has length p. The vector
@@ -220,7 +176,6 @@ fn top_match_candidates(R: &mut Vec<Vec<Complex<f64>>>, p: u16) -> Result<Vec<(u
 
     Ok(final_result)
 }
-
 /// determines how much the camera is rotated counter-clockwise(so return value is always in the range [0, 2pi)) relative to a sky coordinate system (see README)
 /// centered at the determined central star
 /// observed_pattern - the observed flower pattern with star coordinates in the camera coordinate
--- src/coordinates/tests.rs
+++ src/coordinates/tests.rs
@@ -152,6 +152,53 @@ fn long_test() {
         }
     }
 }
+// returns the average distance between the corresponding coordinate axex of the coordinate systems
+fn get_score_of_guess(actual_matrix: Matrix3<f64>, experimental_matrix: Matrix3<f64>) -> f64 {
+    let mut final_distance = 0.0;
+    for col_index in 0..3 {
+        final_distance += actual_matrix.column(col_index).dot(&experimental_matrix.column(col_index));
+    } 
+    final_distance / 3.0
+}
+#[test]
+fn randomized_long_test() {
+    let mut rng = rand::thread_rng();
+    fn get_random_observed_coords(acutal_coords: Vector3<f64>, R_inv: Matrix3<f64>, rng: &mut impl Rng) -> Vector3<f64> {
+        let theoretical_coords = R_inv*acutal_coords;
+        let k = 0.001;
+        let dx = rng.gen_range(-1.0f64..1.0f64)*k;
+        let dy = rng.gen_range(-1.0..1.0)*k;
+        let dz = rng.gen_range(-1.0..1.0)*k;
+        let result = theoretical_coords + Vector3::new(dx, dy, dz);
+        result/result.norm()
+    }
+    let fov = 0.52;
+    let k = 10;
+    let (DFT_db, flower_patterns) = generate_database::generate_db(4.0, k, fov).unwrap();
+    let all_stars: Vec<star::Star> = flower_patterns.iter().map(|fp| fp.central_star).collect();
+    for i in 0..500 {
+        let R = generate_random_orthogonal_matrix();
+        let R_inv = R.try_inverse().unwrap();
+
+        let R_y = R.column(1);
+        let captured_stars: Vec<star::Star> = all_stars
+            .iter()
+            .filter(|&s| s.coords.dot(&R_y) > (1.5*fov).cos())
+            .map(|&s| star::Star {
+                index: 6969, 
+                coords: get_random_observed_coords(s.coords, R_inv, &mut rng),
+                brightness: s.brightness
+            })
+            .collect();
+
+        let experiment_R = get_rotation_matrix(&captured_stars, &DFT_db, &flower_patterns).unwrap();
+        println!("score of guess: {:.5}", get_score_of_guess(R, experiment_R));
+        if i %100 == 0 {
+            println!("real: {}", R);
+            println!("expe: {}", experiment_R);
+        }
+    }
+}
 #[test]
 fn min_distance_between_stars() {
     let all_stars = generate_database::read_hyd_database(4.0, 0.0017).unwrap(); 
